cb6923ee772cb6c6deceb98718597fc61ff7fd0d
pokoot <pokoot@gmail.com>
2017-10-26 03:31:42 +0800

    Fix spacing on test

--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -53,57 +53,56 @@ class VersionTest extends PHPUnit_Framework_TestCase
 
     public function testAbout()
     {
-        $app = $this->app;
+        $app     = $this->app;
         $display = $app['tester']->command('version:about')->execute()->display();
         $this->assertRegExp('/Groovey/', $display);
     }
 
     public function testInit()
     {
-        $app = $this->app;
+        $app     = $this->app;
         $display = $app['tester']->command('version:init')->execute()->display();
         $this->assertRegExp('/Sucessfully/', $display);
     }
 
     public function testReset()
     {
-        $app = $this->app;
+        $app     = $this->app;
         $display = $app['tester']->command('version:reset')->input('Y\n')->execute()->display();
         $this->assertRegExp('/All version entries has been cleared/', $display);
     }
 
     public function testStatus()
     {
-        $app = $this->app;
+        $app     = $this->app;
         $display = $app['tester']->command('version:status')->execute()->display();
         $this->assertRegExp('/Nothing to migrate/', $display);
     }
 
     public function testUp()
     {
-        $app = $this->app;
+        $app     = $this->app;
         $display = $app['tester']->command('version:up')->input('Y\n')->execute()->display();
         $this->assertRegExp('/No new files to be migrated/', $display);
     }
 
     public function testListing()
     {
-        $app = $this->app;
+        $app     = $this->app;
         $display = $app['tester']->command('version:list')->execute()->display();
-
         $this->assertRegExp('/Version | Changelog/', $display);
     }
 
     public function testDown()
     {
-        $app = $this->app;
+        $app     = $this->app;
         $display = $app['tester']->command('version:down')->input('Y\n')->execute()->display();
         $this->assertRegExp('/Nothing to downgrade/', $display);
     }
 
     public function testDrop()
     {
-        $app = $this->app;
+        $app     = $this->app;
         $display = $app['tester']->command('version:drop')->input('Y\n')->execute()->display();
         $this->assertRegExp('/Versions table has been deleted/', $display);
     }
